3493fa753dfff9a8374729d80352e567200a9114
Your Name <igorvasiloi@mail.ru>
2019-02-28 21:05:38 +0200

    class work

---
+++ src/js/index.js
@@ -0,0 +1,10 @@
+var age = 18;
+console.log("my age: ", age);
+console.log("age has type: ", typeof age);
+
+var name = "vitali";
+console.log("my name: ", name);
+console.log("name has type: ", typeof name);
+
+//var name = "Grigorie"//
+//console.log('hello', name);
